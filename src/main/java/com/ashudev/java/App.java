bbda916c6b332e0f3dcb54943a023a3357ba87da
Ashu <pandeyashutoshbusiness@gmail.com>
2022-07-29 15:42:51 +0000

    IN001

---
+++ src/main/java/com/ashudev/java/App.java
@@ -0,0 +1,13 @@
+package com.ashudev.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello I'M Ashu, Welcome to My World" );
+    }
+}
