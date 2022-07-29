96400bbc72b59e426cac56ea75e8681246e860f9
Ashu <pandeyashutoshbusiness@gmail.com>
2022-07-29 16:28:35 +0000

    IN002[C

--- src/main/java/com/ashudev/java/App.java
+++ src/main/java/com/ashudev/java/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello, I'M Ashu, Welcome to My World" );
+        System.out.println( "Hello, I'M Ashu" );
     }
 }
