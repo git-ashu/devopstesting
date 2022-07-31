5cd329bef4961833731a217e4b48fcb264f7c3cb
Ashu <pandeyashutoshbusiness@gmail.com>
2022-07-31 11:31:33 +0000

    changes in App.java file

--- src/main/java/com/ashudev/java/App.java
+++ src/main/java/com/ashudev/java/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello, I'M Ashu from Devloper Team" );
+        System.out.println( "Hello, I'M Ashu from Devloper Team !" );
     }
 }
